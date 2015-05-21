7f280472e253407299387d529352c6670056b618
hashrock <hashedrock@gmail.com>
2015-05-22 00:29:26 +0900

    fix lists

--- study-meteor-vm.js
+++ study-meteor-vm.js
@@ -1,23 +1,35 @@
+Tasks = new Mongo.Collection("task");
+
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
   Template.hello.helpers({
     items: function(){
-      return [
-        {name: "task1"},
-        {name: "task2"}
-      ]
+      return Tasks.find();
     }
   })
   
-  Template.hello.viewmodel({
-    counter: function () {
-      return Session.get('counter');
-    },
-    addCounter: function () {
-      Session.set('counter', Session.get('counter') + 1);
+  Template.form.events({
+    "submit form": function(event){
+      var name = event.target.name.value;
+      Tasks.insert({
+        name: name,
+        created: new Date()
+      });
+      event.target.name.value = "";
+      event.preventDefault();
     }
-  })
+  });
+  
+  Template.item.viewmodel(function(data) { return data; },{
+      item: function(){
+        Tasks.findOne(this._id());
+      },
+      removeItem: function(){
+        Tasks.remove(this._id());
+      }
+  });
 }
 
 if (Meteor.isServer) {
